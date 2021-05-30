f4814aa48a3b97231189510e00836c0f2f6878d3
SanghaviPR123 <77285043+SanghaviPR123@users.noreply.github.com>
2021-05-30 22:47:05 +0530

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,22 @@
+#Miniproject
+# calculator
+"""
+x = float (input("Enter the first Value: "))
+y = float (input("Enter the second Value: "))
+r = (" + - * / ")
+z = input("Enter the symbol of required Operation: + for sum, - for subtraction, * for multiplication, / for division ")
+a=x+y # addition
+b=x-y # subtraction 
+c=x*y # multiplication 
+d=x/y # division 
+if z == "+" :
+    print(a)
+if z == "-" :
+    print(b)
+if z == "*" :
+    print(c)
+if z == "/" :
+    print(d)
+if z not in r :
+    print("invalid input! ")
+"""
